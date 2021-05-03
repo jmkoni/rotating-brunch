ed7d5b9faebf00bb7ac9e9025da6c2625ad58d10
Jennifer Konikowski <jmkoni@icloud.com>
2021-05-03 09:15:14 -0400

    fix typo

--- app/models/slack_message.rb
+++ app/models/slack_message.rb
@@ -4,7 +4,7 @@ module SlackMessage
   def group_message(group:)
     group_usernames = ""
     group_usernames = " <@#{group[0]}>, <@#{group[1]}>, and <@#{group[2]}>" if group.size == 3
-    group_usernames = " <@#{group[0]}>, <@#{group[1]}>, <@#{group[2]}>, and <@#{group[3]}" if group.size == 4
+    group_usernames = " <@#{group[0]}>, <@#{group[1]}>, <@#{group[2]}>, and <@#{group[3]}>" if group.size == 4
     [
       {
         type: "section",
