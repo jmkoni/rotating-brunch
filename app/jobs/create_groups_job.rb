5ea0e4af21e79bd88082e8faf3e0c7776489cd53
Jennifer Konikowski <jmkoni@icloud.com>
2021-05-20 14:15:59 -0400

    OPTIMIZE ALL THE THINGS!!!

--- app/jobs/create_groups_job.rb
+++ app/jobs/create_groups_job.rb
@@ -11,34 +11,18 @@ class CreateGroupsJob < ApplicationJob
 
   def self.group_members(members:)
     groups = []
-    while members.size > 9
-      members, groups = group(members: members, groups: groups, num: 4)
+    members.shuffle!
+    groups << members.shift(4) while members.any?
+    i = -2
+    while groups.last.length < 3
+      groups.last << groups[i].pop
+      i -= 1
     end
-    if members.size == 9
-      3.times { members, groups = group(members: members, groups: groups, num: 3) }
-    elsif members.size == 8
-      2.times { members, groups = group(members: members, groups: groups, num: 4) }
-    elsif members.size == 7
-      members, groups = group(members: members, groups: groups, num: 4)
-      members, groups = group(members: members, groups: groups, num: 3)
-    elsif members.size == 6
-      2.times { members, groups = group(members: members, groups: groups, num: 3) }
-    end
-    groups << members.to_a if members.size > 0
-    groups
   end
 
   def self.start_conversations(groups:)
     groups.each do |group|
       Slack::Client.create_conversation(group: group)
     end
-  end
-
-  private
-  def self.group(members:, groups:, num:)
-    temp_group = members.sample(num)
-    groups << temp_group
-    temp_group.each { |group| members.delete(group) }
-    [members, groups]
   end
 end
