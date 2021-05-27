64d776bd5b1c6f216b29e04fc8e3fee0e0d78f41
Jennifer Konikowski <jmkoni@icloud.com>
2021-05-27 09:47:54 -0400

    fix standardrb issues

--- app/lib/slack/client.rb
+++ app/lib/slack/client.rb
@@ -2,28 +2,30 @@ module Slack
   class Client
     attr_reader :client
 
-    def self.get_channel_users(client: nil)
-      client ||= default_client
-      members = client.conversations_members(
-        channel: ENV["GROUP_CHANNEL"],
-        limit: 10000
-      )
-      members.members
-    end
+    class << self
+      def get_channel_users(client: nil)
+        client ||= default_client
+        members = client.conversations_members(
+          channel: ENV["GROUP_CHANNEL"],
+          limit: 10000
+        )
+        members.members
+      end
 
-    def self.create_conversation(group:)
-      client ||= default_client
-      conv = client.conversations_open(users: group.join(","))
-      client.chat_postMessage(
-        channel: conv.channel.id,
-        blocks: SlackMessage.group_message(group: group)
-      )
-    end
+      def create_conversation(group:)
+        client ||= default_client
+        conv = client.conversations_open(users: group.join(","))
+        client.chat_postMessage(
+          channel: conv.channel.id,
+          blocks: SlackMessage.group_message(group: group)
+        )
+      end
 
-    private
+      private
 
-    def self.default_client
-      ::Slack::Web::Client.new(token: ENV["SLACK_OAUTH_TOKEN"])
+      def default_client
+        ::Slack::Web::Client.new(token: ENV["SLACK_OAUTH_TOKEN"])
+      end
     end
   end
 end
