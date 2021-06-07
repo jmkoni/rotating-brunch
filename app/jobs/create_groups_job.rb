4751d50ee2b29bd2d1a21dd904368efa634733ff
Jennifer Konikowski <jmkoni@icloud.com>
2021-06-07 08:57:47 -0400

    making sandy metz happy

--- app/jobs/create_groups_job.rb
+++ app/jobs/create_groups_job.rb
@@ -1,31 +1,41 @@
 class CreateGroupsJob < ApplicationJob
   MAX_GROUP_SIZE = 4
 
-  def self.perform
-    date = Date.today
-    if date.monday? && date.cweek.odd?
+  class << self
+    def perform
+      date = Date.today
+      if date.monday? && date.cweek.odd?
+        perform!
+      end
+    end
+
+    def perform!
       Rails.logger.info("Running CreateGroupsJob")
       members = Slack::Client.get_channel_users
       groups = group_members(members: members)
       start_conversations(groups: groups)
     end
-  end
 
-  def self.group_members(members:)
-    groups = []
-    members.shuffle!
-    groups << members.shift(MAX_GROUP_SIZE) while members.any?
-    i = -2
-    while groups.last.length < (MAX_GROUP_SIZE - 1)
-      groups.last << groups[i].pop
-      i -= 1
+    def group_members(members:)
+      groups = []
+      members.shuffle!
+      groups << members.shift(MAX_GROUP_SIZE) while members.any?
+      balance_groups(groups)
     end
-    groups
-  end
 
-  def self.start_conversations(groups:)
-    groups.each do |group|
-      Slack::Client.create_conversation(group: group)
+    def balance_groups(groups)
+      i = -2
+      while groups.last.length < (MAX_GROUP_SIZE - 1)
+        groups.last << groups[i].pop
+        i -= 1
+      end
+      groups
+    end
+
+    def start_conversations(groups:)
+      groups.each do |group|
+        Slack::Client.create_conversation(group: group)
+      end
     end
   end
 end
